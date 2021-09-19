5e545f6d586ebfa518985e2d358e6dad36a3a595
Erica Zavaleta <44880722+EntropySynthetica@users.noreply.github.com>
2021-09-19 18:04:03 -0500

    Using Material Table to render the tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import MaterialTable from "material-table";
+import { makeStyles } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SearchIcon from "@material-ui/icons/Search";
+import SaveIcon from "@material-ui/icons/Save";
+import { Button } from "@material-ui/core";
+import "./index.css";
 
 function App() {
 
@@ -13,14 +20,68 @@ function App() {
 
   }, [])
 
+  const useStyles = makeStyles(theme => ({
+    root: {
+      "& .MuiPaper-root": {
+        borderRadius: "15px",
+        width: "90%",
+        boxShadow: "10px 10px 5px 0px rgba(0,0,0,0.75);"
+      }
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
     <div>
-      <ul>
-      {(typeof data === 'undefined') ? (<p>Loading...</p>) :   // Display Loading... until the API can grab data. 
-        (
-          data.map((task, i) => <li key={i}>{task.task_name} / {task.task_desc} / {task.status_name} / </li>)  // Iterate thru the tasks and display them as an unordered list
-        )}
-      </ul>
+      <div class="App">
+        <div className={classes.root}>
+          <MaterialTable
+            columns={[
+              { title: "Name", field: "task_name" },
+              { title: "Description", field: "task_desc" },
+              { title: "Status", field: "status_name" }
+            ]}
+
+            data={data}
+            title="Task List"
+            icons={{
+              Clear: (props) => <DeleteIcon />,
+              Search: (props) => <SearchIcon />,
+              ResetSearch: (props) => <DeleteIcon />
+            }}
+
+            // actions={[
+            //   {
+            //     icon: () => <SaveIcon />,
+            //     tooltip: "Save User",
+            //     onClick: (event, rowData) => alert("You saved " + rowData.name)
+            //   }
+            // ]}
+
+            // components={{
+            //   Action: (props) => (
+            //     <Button
+            //       onClick={(event) => props.action.onClick(event, props.data)}
+            //       color="primary"
+            //       variant="text"
+            //       style={{ textTransform: "none" }}
+            //       size="small"
+            //     >
+            //       Save
+            //     </Button>
+            //   )
+            // }}
+
+            options={{
+              headerStyle: {
+                backgroundColor: "#01579b",
+                color: "#FFF"
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
